d10e0dc0e7d8d658c1462370d3471ac976cdd454
Olga <okold525@mtroyal.ca>
2021-11-08 23:24:38 -0700

    white screen on deploy

--- src/App.js
+++ src/App.js
@@ -67,8 +67,8 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" exact element={<Home isLoading={this.isLoading}/>}/>
-          <Route path="/browse" exact element={<PlayBrowser addToFavs={this.addToFavs} getPlayList={this.getPlayList}/>}/>
+          <Route path="/comp4513-a1" element={<Home isLoading={this.isLoading}/>}/>
+          <Route path="/browse" element={<PlayBrowser addToFavs={this.addToFavs} getPlayList={this.getPlayList}/>}/>
         </Routes>
       </BrowserRouter>
     );
